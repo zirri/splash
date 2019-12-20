3ce045723727114ea69b4b6e1ce7e975b7386bde
zarmel <50460344+zaramelyan@users.noreply.github.com>
2019-12-20 13:13:23 +0100

    Adds server file

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('build'));
+
+const port = process.env.PORT;
+const secret = process.env.SECRET;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, 
+});
+
+
+
+
+
+app.listen(3000)
+console.log('Running on port 3000');
