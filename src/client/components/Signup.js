c05ce58b5e02d19a75f66f95730974ba27b44ad1
zarmel <50460344+zaramelyan@users.noreply.github.com>
2020-01-06 13:30:05 +0100

    Cleans App.js

---
+++ src/client/components/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+class Signup extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Signup
+
