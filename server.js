7667e3262afa9174a3f74282da2e5c1d5e69a8ce
zirri <as.borander@gmail.com>
2020-01-06 16:17:56 +0100

    initial files API and serving static data

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const sessionApi = require('./src/server/sessionApi.js');
 const userApi = require('./src/server/userApi.js');
-const waterUsageApi = require('./src/server/wasterUsageApi.js');
+const waterUsageApi = require('./src/server/waterUsageApi.js');
 const app = express();
 
 //Middleware
@@ -12,13 +12,18 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next)=>{
+  const { method, path } = req;
+  console.log(`New request of type ${method} to endpoint '${path}'`)
+  next();
+})
 //Serving static files
 app.use(express.static('build'));
 
 //Routing
-app.use('/user', userApi);
-app.use('/session', sessionApi);
-app.use('/waterusage', waterUsageApi);
+app.use('/api/user', userApi);
+app.use('/api/session', sessionApi);
+app.use('/api/waterusage', waterUsageApi);
 
 //Listening to port
 const port = process.env.PORT;
--- src/server/services/databaseServices.js
+++ src/server/services/databaseServices.js
@@ -3,3 +3,76 @@ const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL, 
   });
+
+const users = [
+  {
+    userid:1,
+    username: 'blobb',
+    password: 'passordblobb',
+    email: 'blobb@123.no'
+  },
+  {
+    userid:2,
+    username: 'blubb',
+    password: 'passordblubb',
+    email: 'blubb@123.no'
+  }
+];
+
+const waterUsage = [
+  {
+    id: 0,
+    userid: 1,
+    date: "2020-01-01 12:22",
+    amount: 4,
+    room: 'bathroom',
+    source: 'sink'
+  },
+  {
+    id: 1,
+    userid: 1,
+    date: "2020-01-02 12:52",
+    amount: 3,
+    room: 'bathroom',
+    source: 'shower'
+  },
+  {
+    id: 2,
+    userid: 1,
+    date: "2020-01-01 14:44",
+    amount: 4,
+    room: 'kitchen',
+    source: 'sink'
+  },
+  {
+    id: 3,
+    userid: 2,
+    date: "2020-01-01 14:44",
+    amount: 1,
+    room: 'kitchen',
+    source: 'sink'
+  },
+  {
+    id: 4,
+    userid: 2,
+    date: "2020-01-01 14:44",
+    amount: 1,
+    room: 'bathroom',
+    source: 'sink'
+  }
+];
+
+function getUserInformation(userid){
+  const user = users.filter(user => user.userid == userid);
+  return user[0];
+}
+
+function getWaterUsage(userid){
+  const waterUsageUser = waterUsage.filter(record => record.userid == userid);
+  return waterUsageUser;
+}
+
+module.exports = {
+  getUserInformation,
+  getWaterUsage
+}
