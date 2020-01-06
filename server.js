ca4bb594e34f109009310a2223000708e3e241bd
zirri <as.borander@gmail.com>
2020-01-06 16:34:24 +0100

    set up get-requests for water usage and user

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use((req, res, next)=>{
   console.log(`New request of type ${method} to endpoint '${path}'`)
   next();
 })
+
 //Serving static files
 app.use(express.static('build'));
 
--- src/server/services/databaseServices.js
+++ src/server/services/databaseServices.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const { Pool } = require('pg');
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL, 
