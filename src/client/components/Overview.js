8c51deea74bdbfe19106a8782814b69188482727
zarmel <50460344+zaramelyan@users.noreply.github.com>
2020-01-07 14:34:21 +0100

    small commit

--- src/client/components/Overview.js
+++ src/client/components/Overview.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 import Today from './Today';
 import Week from './Week';
 
 
 
 class Overview extends React.Component {
+    
  
     render() {
         return (
-            <div>
-                <br />
+            <main>
                 Overview
             <p>Today</p>
             <Link to='/today'>Today</Link>
             <p>This week</p>
-            </div>
-            
+            </main>
         )
     }
 }
