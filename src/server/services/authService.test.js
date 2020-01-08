e1479f6e3d687eb345309fb98c5cdff655a6cf0e
zirri <as.borander@gmail.com>
2020-01-08 14:00:38 +0100

    setup testing database service

--- src/server/services/authService.test.js
+++ src/server/services/authService.test.js
@@ -1,6 +1,28 @@
 const { logInAndGetToken } = require('./authService.js');
 
+
 test('valid login', async () => {
-    const result = await logInAndGetToken('123@test.com', 'password123');
+    let result = await logInAndGetToken('123@test.com', 'password123');
     expect(result).toMatchObject({status: 200});
+});
+
+test('invalid login', async () => {
+    let result = await logInAndGetToken('123@test.com', 'notValidPassword');
+    expect(result).toMatchObject({status: 401});
+    result = await logInAndGetToken('notValidMail', 'notValidPassword');
+    expect(result).toMatchObject({status: 401});
+});
+
+test('invalid datatype input', async () => {
+    let result = await logInAndGetToken(1,7);
+    expect(result).toMatchObject({status: 401});
+    result = await logInAndGetToken([], 'password');
+    expect(result).toMatchObject({status: 401});
+});
+
+test('missing input', async () => {
+    let result = await logInAndGetToken('notValidEmail');
+    expect(result).toMatchObject({status: 401});
+    result = await logInAndGetToken();
+    expect(result).toMatchObject({status: 401});
 });
