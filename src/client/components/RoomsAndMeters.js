fdd233198eab8cc83bdbc6b31003a2a5827f2ab4
cathynguyen <c4thyng@github.com>
2020-01-08 10:31:38 +0100

    Added Header.Component. Modified Navigation.Component

--- src/client/components/RoomsAndMeters.js
+++ src/client/components/RoomsAndMeters.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
 
+import { Button, ListGroup } from "react-bootstrap";
+
+import { Link } from 'react-router-dom';
+
+import Header from './Header';
 
 class RoomsAndMeters extends React.Component {
-    render() {
-        return (
-            <div>Rooms and Water meters</div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <h2>Rooms and Water meters</h2>
+        <ListGroup variant="flush">
+          <ListGroup.Item>Cras justo odio</ListGroup.Item>
+          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+        </ListGroup>
+
+        <Button variant="primary" size="sm">
+          <Link to="/addMeter" style={{color:"white"}}>ADD WATER METER</Link>
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default RoomsAndMeters;
