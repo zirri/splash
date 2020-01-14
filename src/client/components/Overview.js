1b94a33a3801795381d1e57b3c9749e8b111e873
zarmel <50460344+zaramelyan@users.noreply.github.com>
2020-01-14 11:41:00 +0100

    fixed data reg layout

--- src/client/components/Overview.js
+++ src/client/components/Overview.js
@@ -14,6 +14,8 @@ import {
   Jumbotron,
   Container,
   Button, 
+  Col,
+  Row
 } from "react-bootstrap";
 
 //REACT-CHARTJS-2
@@ -501,15 +503,29 @@ class Overview extends React.Component {
             </Container>
           </Tab>
           <Tab eventKey="register" title="REGISTER">
-          <h2>Simulate water usage</h2>
-                <Button onClick={this.handleRegisterClick.bind(this, 75, 123456)}>Take a 5 min shower (75L) </Button>
-                <Button onClick={this.handleRegisterClick.bind(this, 150, 123456)}>Take a 10 min shower (150L)</Button>
-                <Button onClick={this.handleRegisterClick.bind(this, 37.5, 123456)}>Take a 5 min shower using 'sparedusj' (37,5L)</Button>
-                <Button onClick={this.handleRegisterClick.bind(this, 20, 123321)}>Run the washing machine (20L)</Button>
-                <Button onClick={this.handleRegisterClick.bind(this, 20, 123987)}>Use the dishwasher (20L)</Button>
-                <Button onClick={this.handleRegisterClick.bind(this, 200, 123456)}>Take a bath (200L)</Button>
-                <Button onClick={this.handleRegisterClick.bind(this, 6, 123789)}>Let water run for 30s to get cold water (6L)</Button>
-                <Button onClick={this.handleRegisterClick.bind(this, 6, 123111)}>Flush the WC (6L)</Button>      
+            <main className="meters-align">
+            <Row className="justify-content-md-center meters-align">
+              <h2>Simulate water usage</h2>
+              </Row>
+              <Row className="justify-content-md-center meters-align">
+            <Col xs lg="3">
+                
+                <p className="data-registration">Bathroom</p>
+                  <Button className="data-registration" onClick={this.handleRegisterClick.bind(this, 75, 123456)}>Take a 5 min shower (75L) </Button>
+                  <Button  className="data-registration" onClick={this.handleRegisterClick.bind(this, 150, 123456)}>Take a 10 min shower (150L)</Button>
+                  <Button  className="data-registration" onClick={this.handleRegisterClick.bind(this, 37.5, 123456)}>Take a 5 min shower using 'sparedusj' (37,5L)</Button>
+                  <Button  className="data-registration" onClick={this.handleRegisterClick.bind(this, 200, 123456)}>Take a bath (200L)</Button>
+                  <Button  className="data-registration" onClick={this.handleRegisterClick.bind(this, 6, 123111)}>Flush the WC (6L)</Button>    
+            </Col>
+
+            <Col xs lg="3">
+                <p className="data-registration">Kitchen</p>
+                  <Button  className="data-registration" onClick={this.handleRegisterClick.bind(this, 20, 123321)}>Run the washing machine (20L)</Button>
+                  <Button  className="data-registration" onClick={this.handleRegisterClick.bind(this, 20, 123987)}>Use the dishwasher (20L)</Button>
+                  <Button  className="data-registration" onClick={this.handleRegisterClick.bind(this, 6, 123789)}>Let water run for 30s to get cold water (6L)</Button>
+            </Col>
+            </Row>
+            </main>
           </Tab>
         </Tabs>
       </>
